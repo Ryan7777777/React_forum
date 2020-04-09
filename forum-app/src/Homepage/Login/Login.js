465954f402d6f3a44a3a761c708e782fa810b86f
Ryan7777777 <ryanleung7777@gmail.com>
2020-04-09 17:17:45 +1200

    focus email input

--- forum-app/src/Homepage/Login/Login.js
+++ forum-app/src/Homepage/Login/Login.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {Button,Modal,Form, FormControl} from 'react-bootstrap';
 import './Login.css';
 import {userService} from "../../_services/user.service.js";
+import $ from 'jquery';
 class Login extends React.Component{
     constructor(props){
         super(props)
-        this.state = {inCorrect: null, email : '',password : ''}
+        this.state = {inCorrect: null, email : '',password : '',show: false}
     }
     loging = () => {
         this.setState({email: '',password: ''}) 
@@ -17,7 +18,7 @@ class Login extends React.Component{
        this.setState({password:password.target.value})
     }
     closeModal = () => {
-        this.setState({email: '',password: ''})
+        this.setState({email: '',password: '',show: false})
         this.props.toggleModal()
     }
     register = () =>{
@@ -33,28 +34,35 @@ class Login extends React.Component{
             this.setState({inCorrect: true,password: ''})
         }
     }
+    componentDidUpdate() {
+        if(this.props.show_d===true && this.state.show === false){
+            this.state.show = true
+            this._input.focus();
+        }
+      }
     render(){
         return(
-            <Modal show={this.props.show_d} ref={this.props.modalref} centered = {'true'} size={'md'} onHide={this.closeModal} >
+            <Modal  dialogClassName="modal" autoFocus={false} show={this.props.show_d} centered = {'true'} size={'md'} onHide={this.closeModal} >
             <Modal.Header closeButton>
             <Modal.Title >User Login</Modal.Title>
             </Modal.Header>
+            <Form onSubmit={this.handleSubmit}>
             <Modal.Body>
-            <Form validated={false} onSubmit={this.handleSubmit}>
             <Form.Label>User Name</Form.Label>
             <Form.Group controlId="login_email">
-            <Form.Control  type="email" value={this.state.email} onChange = {this.updateemail} placeholder="Enter email"required/>
+            <Form.Control autoFocus={true} className="focusedInput"stype="email" value={this.state.email} onChange = {this.updateemail} placeholder="Enter email" required ref={c => (this._input = c)}/>
             </Form.Group>
             <Form.Group controlId="userPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" isInvalid={this.state.inCorrect} value={this.state.password} onChange = {this.updatepassword} placeholder="Password" required/>
             <FormControl.Feedback type="invalid">Invalid email address or password</FormControl.Feedback>
             </Form.Group>
-            <br/>
-            < Button  className="position-relative col-3 col-sm-2" type="submit" variant='outline-success' >Login</Button>
-            < Button  className="position-relative register col-3 col-sm-2" variant='outline-secondary' onClick={this.register}>Register</Button>  
-            </Form>
             </Modal.Body>
+            <Modal.Footer>
+            < Button  className="position-relative col-3 col-sm-2 btn" type="submit" variant='outline-success' >Login</Button>
+            < Button  className="position-relative register col-3 col-sm-2 btn" variant='outline-secondary' onClick={this.register}>Register</Button>  
+            </Modal.Footer>
+            </Form>
         </Modal>
         )
     }
