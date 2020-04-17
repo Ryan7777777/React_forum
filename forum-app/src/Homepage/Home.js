859c3265899c664ea5a7cbc7b54743be4a11c609
Ryan7777777 <ryanleung7777@gmail.com>
2020-04-17 21:47:48 +1200

    renderMemberPage

--- forum-app/src/Homepage/Home.js
+++ forum-app/src/Homepage/Home.js
@@ -46,6 +46,12 @@ class Home extends React.Component{
             document.getElementById('slidebar').style.width="0%";
         }
     }
+    renderMemberPage = ()=>{
+        if(localStorage.login === "true"){
+            return(<Member show_MemberModal = {this.state.memberModal} toggleMemberModal={this.toggleMemberModal} user_id={this.state.user_id}/>)
+        } else{
+        }
+    }
     render(){
         return(
             <div>
@@ -55,7 +61,7 @@ class Home extends React.Component{
                 </div>
                 <div className="slidewrapper" id="slidebar">
                     <Login show_LoginModal = {this.state.loginModal} toggleLoginModal={this.toggleLoginModal} login={this.state.login}/>
-                    <Member show_MemberModal = {this.state.memberModal} toggleMemberModal={this.toggleMemberModal} user_id={this.state.user_id}/>
+                    {this.renderMemberPage}
                     <SideNav show_slide = {this.state.slidebar} toggleLoginModal={this.toggleLoginModal} toggleMemberModal={this.toggleMemberModal} />
                 </div>
             </div>
