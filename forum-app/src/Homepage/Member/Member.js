34aa2c0cc48b9c0ffeefd5803b9024ad81dd4ece
Ryan7777777 <ryanleung7777@gmail.com>
2020-04-21 16:06:10 +1200

    member pege upload user images modual

--- forum-app/src/Homepage/Member/Member.js
+++ forum-app/src/Homepage/Member/Member.js
@@ -1,31 +1,72 @@
 import React from 'react';
 import  './Member.css';
-import {Modal,Form,Button, Row,Col} from 'react-bootstrap';
+import {Modal,Form,Button,Image} from 'react-bootstrap';
 import {userService} from "../../_services/user.service.js";
+import {userPhotoService} from "../../_services/userphoto.service.js";
 class Member extends React.Component{
     constructor(props){
         super(props)
         var res = userService.getuserinfo(localStorage.userId);
         if(res.status === 200){
-            this.state = {userName:res.responseJSON.username,email:res.responseJSON.email,firstName:res.responseJSON.first_name,lastName:res.responseJSON.last_name}
+            this.state = {selectedFile:null,
+                          preview:null,
+                          uploadButtonDisable: true,
+                          deleteButtonDisable: true,
+                          userNameUpdataButtonDisable: true,
+                          userName:res.responseJSON.username,
+                          email:res.responseJSON.email,
+                          firstName:res.responseJSON.first_name,
+                          lastName:res.responseJSON.last_name}
         }
     }
     toggleModal = () =>{
         this.props.toggleMemberModal()
         var res = userService.getuserinfo(localStorage.userId);
         if(res.status === 200){
-            this.setState({userName:res.responseJSON.username,email:res.responseJSON.email,firstName:res.responseJSON.first_name,lastName:res.responseJSON.last_name})
+            this.setState({selectedFile:null,
+                           preview:null, 
+                           uploadButtonDisable: true,
+                           deleteButtonDisable: true,
+                           userNameUpdataButtonDisable: true,
+                           userName:res.responseJSON.username,
+                           email:res.responseJSON.email,
+                           firstName:res.responseJSON.first_name,
+                           lastName:res.responseJSON.last_name})
         }
     }
     updateUserName = (name) =>{
-        this.setState({userName:name.target.value})
+        this.setState({userName:name.target.value,userNameUpdataButtonDisable:false})
     }
     changeUserName = (action) =>{
         action.preventDefault();
         var res = userService.updateUserName(localStorage.userId,this.state.userName)
         if (res.status === 200){
-            alert('Update succeddful!')
-            this.toggleModal();
+            alert('Update successful!')
+        }
+    }
+    fileChangedHandler = (event) => {
+        this.setState({selectedFile: event.target.files[0],preview: URL.createObjectURL(event.target.files[0]),uploadButtonDisable:false,deleteButtonDisable:false})
+      }
+    uploadHandler = () =>{
+        var result = userPhotoService.uploadFile(this.state.selectedFile)
+        if(result.status===200 || result.status === 201){
+            this.setState({selectedFile:null,preview:null, uploadButtonDisable: true,deleteButtonDisable: true})
+            alert("Uploaded successful!")
+        } else{
+            alert(result.statusText)
+        }
+    }
+    deletePteviewImage = () =>{
+        this.setState({selectedFile:null,preview:null,uploadButtonDisable:true,deleteButtonDisable:true})
+    }
+    componentDidUpdate(){
+        if(this.props.show_MemberModal === true && this.state.preview === null){
+            userPhotoService.getUserPhoto(localStorage.userId)
+            userPhotoService.getUserImage(localStorage.userId)
+        } 
+        if(this.state.preview!==null){
+            var logo = document.getElementById("imgImage")
+            logo.src = (this.state.preview);
         }
     }
     render(){
@@ -34,44 +75,27 @@ class Member extends React.Component{
             <Modal.Header closeButton>
                 <Modal.Title>Member Daetails</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-            <Form onSubmit={this.changeUserName}>
-                    <Form.Label>User name</Form.Label>  
-                    <Row>
-                        <Col xs={9}>
-                            <Form.Control className="formUsername" type="string" placeholder={this.state.userName} value={this.state.userName} onChange = {this.updateUserName} maxLength={15}></Form.Control>
-                        </Col>
-                        <Col xs={3}>
-                            <Button varianr='primary' type="submit"> Update</Button>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                             <Form.Label className="formEmail">User email</Form.Label>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12}>
-                            <Form.Label>{this.state.email}</Form.Label> 
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <Form.Label  className="formName"> User's first name</Form.Label>
-                        </Col>
-                        <Col xs={6}>
-                            <Form.Label  className="formName"> User's family  name</Form.Label>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <Form.Label> {this.state.firstName}</Form.Label>
-                        </Col>
-                        <Col xs={6}>
-                            <Form.Label> {this.state.lastName}</Form.Label>
-                        </Col>
-                    </Row>
+            <Modal.Body className="modalbody">
+                <div className ="imageFrame">
+                <label for="photo-upload" className="custom-file-upload fas">
+                <div className="img-wrap img-upload" >
+                <Image className='userImage'for="photo-upload" id="imgImage"/>
+                </div>
+                <input id="photo-upload" type="file" onChange={this.fileChangedHandler}/> 
+                </label>
+                </div>
+                <Button variant="primary"  onClick={this.uploadHandler} className="imageUpLoadButton" disabled ={this.state.uploadButtonDisable}>Upload</Button>
+                <Button variant="danger" onClick={this.deletePteviewImage} className="imageDeleteButton"disabled ={this.state.deleteButtonDisable}>Delete</Button>
+            <Form onSubmit={this.changeUserName} className="form">
+                <Form.Label className="formUserNameLabel">User name</Form.Label>  
+                <Form.Control className="formUsername" type="string" placeholder={this.state.userName} value={this.state.userName} onChange = {this.updateUserName} maxLength={15}></Form.Control>
+                <Button variant='primary' type="submit" className="formUserNameUpdateButton"  disabled ={this.state.userNameUpdataButtonDisable}> Update</Button>
             </Form>
+            <p className="userEmailTitle">User email:</p>
+            <p className="userEmail">{this.state.email}</p> 
+            <p  className="userFullNameTitle"> User's full name:</p>
+            <p className="userFullName"> {this.state.firstName} {this.state.lastName}</p>
+           
             </Modal.Body>
             <Modal.Footer>
             </Modal.Footer>
--- forum-app/src/_services/userphoto.service.js
+++ forum-app/src/_services/userphoto.service.js
@@ -1,5 +1,26 @@
 import $ from 'jquery';
 import defaultUserImage from '../../src/Image/logo/default-user-image.png';
+
+const getUserImage = (userId) =>{
+    $.ajax({
+        url:"http://127.0.0.1:4255/api/v1/users/"+userId+"/photo",
+        cache:false,
+        xhr:function(){// Seems like the only way to get access to the xhr object
+            var xhr = new XMLHttpRequest();
+            xhr.responseType= 'blob'
+            return xhr;
+        },
+        success: function(data){
+            var logo = document.getElementById('imgLogo');
+            var url = window.URL || window.webkitURL;
+            logo.src = url.createObjectURL(data);
+        },
+        error:function(){    
+            var logo = document.getElementById("imgLogo")
+            logo.src = (defaultUserImage);
+        }
+    });
+}
 const getUserPhoto = (userId) =>{
     $.ajax({
         url:"http://127.0.0.1:4255/api/v1/users/"+userId+"/photo",
@@ -10,16 +31,32 @@ const getUserPhoto = (userId) =>{
             return xhr;
         },
         success: function(data){
-            var img = document.getElementById('img');
+            var userPhoto = document.getElementById('imgImage');
             var url = window.URL || window.webkitURL;
-            img.src = url.createObjectURL(data);
+            userPhoto.src = url.createObjectURL(data);
         },
         error:function(){    
-            var img = document.getElementById("img")
-            img.src = (defaultUserImage);
+            var userPhoto = document.getElementById("imgImage")
+            userPhoto.src = (defaultUserImage);
         }
     });
 }
+const uploadFile = (file) =>{
+    return $.ajax({
+        url:"http://127.0.0.1:4255/api/v1/users/"+localStorage.userId+"/photo",
+        data: file,
+        type:'POST',
+        async: false,
+        processData: false,
+        contentType: file.type,
+        headers:{
+            'X-Authorization': localStorage.auth
+        },
+    })
+}
 export const userPhotoService = {
-    getUserPhoto
+    getUserImage,
+    getUserPhoto,
+    uploadFile
+
 };
