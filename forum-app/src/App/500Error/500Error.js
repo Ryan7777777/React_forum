ffd63e67b83b52c9f2bbcc7bb1f5bf1a46628c12
Ryan7777777 <ryanleung7777@gmail.com>
2020-04-28 17:53:36 +1200

    500 error page

---
+++ forum-app/src/App/500Error/500Error.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Error from './500Error.css';
+import './500Error.css';
+function Error500Page() {
+      return (<p>500 Internal Server Error!!!</p>);
+}
+export default Error500Page;
