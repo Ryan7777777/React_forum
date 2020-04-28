060143629c52d02a3f02baa2e01748b7a931596b
Ryan7777777 <ryanleung7777@gmail.com>
2020-04-28 17:57:52 +1200

    body between home page  comment, post dislogue

--- forum-app/src/Homepage/Body/Body.js
+++ forum-app/src/Homepage/Body/Body.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import "./Body.css";
+import  Post  from "./Post/Post";
+import Comment from './Comment/Comment';
 class Body extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {postId:null}
+    }
+    shouldComponentUpdate() {
+        return true;
+    }
+    reportError = () => {
+        this.props.error()
+    }
+    renderComment(){
+        if(this.state.postId !== null){
+            return (<Comment comment_Id = {this.state.postId}/>)
+        }
+    }
+    openNewPost = (newPostId) =>{
+        this.setState({postId:newPostId})
+    }
     render(){
-        return <div>
-        </div>
+        return (
+            <div className = "bodyWrapper">
+               <Comment postId = {this.state.postId}/>
+                <Post newPostId={this.openNewPost}/>
+            </div>)
     }
 }
 export default Body;
