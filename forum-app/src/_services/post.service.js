29416b64e3d699e2ac0ab04d4145e506bbfe3675
Ryan7777777 <ryanleung7777@gmail.com>
2020-04-28 17:55:25 +1200

    add 404, 500 error and use promise on ajax call

--- forum-app/src/_services/post.service.js
+++ forum-app/src/_services/post.service.js
@@ -1,32 +1,26 @@
 import $ from 'jquery';
-import lastUpdateCalaulater  from "../tools/lastupdateCalculater"
-import { commentServie } from "../_services/comment.service"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import lastUpdateCalaulater  from "../tools/lastupdateCalculater";
+import { commentServie } from "../_services/comment.service";
+import Error500Page from '../App/500Error/500Error';
+import Error404Page from '../App/404Error/404Error';
 const loadallpost = () =>{
-    $.ajax({
-        url:'http://127.0.0.1:4255/api/v1/allpost',
-        type:'GET',
-        async: true ,
-        success: function(data){  
-           for (var i=0 ; i < data.length ; i++){
-               var postContainer = document.createElement('div')
-               postContainer.addEventListener('click',function(index){ return function(){commentServie.loadAllComment(data[index].id)}}(i))
-               postContainer.className = "postBox"
-               var author =  document.createElement('p')
-               author.className ="author"
-               author.innerHTML = data[i].author
-               postContainer.appendChild(author)
-               var date = document.createElement('p')
-               date.className = "date"
-               date.innerHTML = lastUpdateCalaulater(data[i].date)
-               postContainer.appendChild(date)
-               var topic = document.createElement('p')
-               topic.innerHTML = data[i].title
-               topic.className = 'title'
-               postContainer.appendChild(topic)
-               var postMenu = document.getElementById('topicMenu');  
-               postMenu.appendChild(postContainer)
-           }
-        }
+    return new Promise((reslove,reject)=>{
+        $.ajax({
+            url:'http://127.0.0.1:4255/api/v1/allpost/',
+            type:'GET',
+            async: true ,
+            success: function(data){   
+                reslove(data)
+            }, error: function(qxhr){
+                if(qxhr.status === 404){
+                    ReactDOM.render(<Error404Page/>,document.getElementById('page'));
+                } else{
+                    ReactDOM.render(<Error500Page/>,document.getElementById('page'));
+                }
+            }
+            })  
     })
 }
 export const postService = {
