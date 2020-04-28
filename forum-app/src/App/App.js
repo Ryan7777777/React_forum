223287360cd1c7945df859560810bbfdacd194a0
Ryan7777777 <ryanleung7777@gmail.com>
2020-04-28 18:00:18 +1200

    redirect init url to comment and error page

--- forum-app/src/App/App.js
+++ forum-app/src/App/App.js
@@ -1,21 +1,30 @@
-import Home from '../Homepage/Home.js';
-import Signup from './Signup/Signup.js'
+import Home from '../Homepage/Home';
+import Signup from './Signup/Signup';
+import Error404Page from './404Error/404Error';
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route,Redirect} from 'react-router-dom';
 function App() {
   return (
-    <div className="page-wrapper">
+    <div className="page-wrapper" id="page">
       <Router>
      <Switch>
             <Route path="/signup/">
               <Signup/>
             </Route>
-            <Route path="/">
+            <Route exact path="/" component={() => <Redirect to="/comment/all" />}/>
+            <Route path="/comment">
               <div className="mainpage">
                  <Home/>
               </div>
             </Route>
+            <Route path="/post">
+              <div className="mainpage">
+                 <Home/>
+              </div>
+            </Route>
+              <Error404Page/>
+            <Route/>
       </Switch>
       </Router>
     </div>
